afb297b1936149e2520d54931855f2702b7b5237
Ruslan Krasulia <krasulia.r.i@gmail.com>
2023-06-05 20:40:30 +0300

    configs

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,23 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@navigation': './src/navigation',
+          '@screen': './src/screen',
+          '@utils': './src/utils',
+          '@context': './src/context',
+          '@api': './src/api',
+          '@hooks': './src/hooks',
+        },
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ],
 };
